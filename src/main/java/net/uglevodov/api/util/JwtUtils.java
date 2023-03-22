955c88196fb111eaf217914de81cf64f4d587712
Leslie <leslie.fernsby@gmail.com>
2023-03-21 21:03:36 -0400

    fixed jwt format

--- src/main/java/net/uglevodov/api/util/JwtUtils.java
+++ src/main/java/net/uglevodov/api/util/JwtUtils.java
@@ -1,4 +1,5 @@
 package net.uglevodov.api.util;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -8,11 +9,9 @@ import net.uglevodov.api.model.UserRole;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
 
 @Component
 public class JwtUtils {
@@ -35,15 +34,13 @@ public class JwtUtils {
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", user.getRole());
 
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expirationInSeconds * 1000))
                 .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret().getBytes(StandardCharsets.UTF_8))
                 .compact();
-
-        return Base64.getUrlEncoder().encodeToString(jwt.getBytes(StandardCharsets.UTF_8));
     }
 
     public String getUsernameFromToken(String token) {
@@ -65,10 +62,9 @@ public class JwtUtils {
     }
 
     private Claims getClaimsFromToken(String token) {
-        String decodedJwt = new String(Base64.getUrlDecoder().decode(token), StandardCharsets.UTF_8);
         return Jwts.parser()
                 .setSigningKey(jwtConfig.getSecret().getBytes(StandardCharsets.UTF_8))
-                .parseClaimsJws(decodedJwt)
+                .parseClaimsJws(token)
                 .getBody();
     }
 }
